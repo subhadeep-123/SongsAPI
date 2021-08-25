c831367c85dc039af8228d78834a0234e4150ca1
Subhadeep Banerjee <subhadeep762@gmail.com>
2021-08-25 05:43:20 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+from os import urandom
 class Config(object):
     '''
     Each environment will be a class that inherits from the main class config
@@ -5,16 +6,17 @@ class Config(object):
     Configurations that will be the same across all environment will go into config,
     while configuration that are specific to an environment will go into the relevant environment below
     '''
-    DEBUG = True
-    SECRET_KEY = "YQ2-_wtF~K;S4#nULjV-E@`&Xz@;%Aq81.mn7vWrzF3WJ5P1OvEAf<^(Ov068B"
+    SECRET_KEY = urandom(32)
 
 
 class Production(Config):
+    DEBUG = False
     FLASK_ENV = 'production'
     PRODUCTION = True
 
 
 class Testing(Config):
+    DEBUG = False
     FLASK_ENV = 'testing'
     TESTING = True
 
